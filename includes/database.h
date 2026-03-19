e82568d15169f8cf90326f2bdba127050f0deec4
cin-tie <csizegod@gmail.com>
2026-03-19 20:45:04 +0300

    Writed header

---
+++ includes/database.h
@@ -0,0 +1,71 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+
+#include <sqlite3>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Структуры данных
+typedef struct {
+    int id;
+    char address[256];
+    char phone[64];
+    char car_brands[512];
+} Workshop;
+
+typedef struct {
+    int id;
+    char name[256];
+    char specialization[128];
+    int workshop_id;
+} Mechanic;
+
+typedef struct {
+    char license_plate[16];
+    char brand[64];
+    char model[64];
+    int year;
+    char owner_name[256];
+    char passport_number[64];
+    char owner_address[256];
+} Car;
+
+typedef struct {
+    int id;
+    char name[256];
+    char category[128];
+    float base_price;
+} RepairType;
+
+typedef struct {
+    int id;
+    int workshop_id;
+    int mechanic_id;
+    char car_license[16];
+    int repair_type_id;
+    char start_date[16];
+    char end_date[16];
+    float cost;
+    char status[32];
+} Repair;
+
+// Инициализация базы данных
+sqlite3* db_init(const char* db_name);
+
+// Закрытие базы данных
+void db_close(sqlite3* db);
+
+// Выполнение SQL запроса без возврата данных
+int db_execute(sqlite3* db, const char* sql);
+
+// Выполнение запроса с возвратом данных (callback)
+int db_query(sqlite3* db, const char* sql, int (*callback)(void*,int,char**,char**), void* data);
+
+// Получение последнего ID вставленной записи
+int db_last_insert_rowid(sqlite3* db);
+
+// Экранирование строки для SQL (защита от SQL injection)
+char* db_escape_string(const char* str);
+
+#endif
